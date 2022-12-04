c97a49ca9d9c5574977583dee94e5d35a907dfba
YongAn <414675056@qq.com>
2022-12-04 19:41:48 +0800

    添加项目文件。

---
+++ demoForWpfApp60/App.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace demoForWpfApp60
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public IServiceProvider Services { get; }
+
+        public App()
+        {
+            InitializeComponent();
+            Services = ConfigureServices();
+        }
+
+        private static IServiceProvider ConfigureServices()
+        {
+            var services = new ServiceCollection();
+            services.AddSingleton<MainWindow>();
+            return services.BuildServiceProvider();
+        }
+
+        private void OnStartup(object sender, StartupEventArgs e)
+        {
+            var mainWindow = Services.GetService<MainWindow>();
+            mainWindow?.Show();
+        }
+    }
+}
